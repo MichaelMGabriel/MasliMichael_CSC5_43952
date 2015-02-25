44df545ffb4ebce1b71c8c62251d290623099290
MichaelMGabriel <michael_masli@hotmail.com>
2015-02-25 10:43:06 -0800

    Signed-off-by: MichaelMGabriel <michael_masli@hotmail.com>

---
+++ Lab/HelloWorld2ndF/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on February 25, 2015, 10:41 AM
+ * Purpose: Wasn't satisfied with 1 F
+ *          I wanted another
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ Lab/HelloWorldGradeF/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on February 25, 2015, 10:39 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"HelloWorld"<<endl;
+    return 0;
+}
+
